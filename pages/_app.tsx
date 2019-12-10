afc2c3e2ed832b83af6a8dcb2ec46886e68ac38c
Omar Jbara <omar.jbara2@gmail.com>
2019-12-11 00:35:09 +0100

    - Removed useless getInitialProps on _app - Silented NOW bot (sorry love u)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,6 @@ import AppNav from '~/components/layout/AppNav'
 import { MercureProvider } from '@liinkiing/use-mercure'
 
 class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
