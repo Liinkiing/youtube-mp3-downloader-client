863112bb80bb7b77a1239b302281d2ec2fac35c7
Omar Jbara <omar.jbara2@gmail.com>
2019-12-10 03:55:42 +0100

    - Tweaks for useMercure hook

--- src/hooks/useMercure.ts
+++ src/hooks/useMercure.ts
@@ -1,9 +1,9 @@
-import { DependencyList, useContext, useEffect } from 'react'
+import { DependencyList, useContext, useEffect, useMemo } from 'react'
 import { MercureContext } from '~/providers/mercure'
 
 const useMercure = <D>(topic: string, onData: (data: D) => void, deps: DependencyList = []) => {
   const hubUrl = useContext(MercureContext)
-  const eventSource = new EventSource(`${hubUrl}?topic=${encodeURIComponent(topic)}`)
+  const eventSource = useMemo(() => new EventSource(`${hubUrl}?topic=${encodeURIComponent(topic)}`), [hubUrl, topic])
   useEffect(() => {
     eventSource.onmessage = event => {
       try {
@@ -12,11 +12,15 @@ const useMercure = <D>(topic: string, onData: (data: D) => void, deps: Dependenc
         onData(event.data as D)
       }
     }
-
+    return () => {
+      eventSource.onmessage = null
+    }
+  }, deps)
+  useEffect(() => {
     return () => {
       eventSource.close()
     }
-  }, [...deps])
+  }, [])
 }
 
 export default useMercure
