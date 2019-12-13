a12b11350c74d0885d9e9e638400f4c738c5584f
Omar Jbara <omar.jbara2@gmail.com>
2019-12-13 01:42:44 +0100

    - Correct SSR styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,14 +7,28 @@ interface Props {
 }
 
 export default class MyDocument extends Document<Props> {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(ctx)
 
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-
-    const styleTags = sheet.getStyleElement()
-
-    return { ...page, styleTags }
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
