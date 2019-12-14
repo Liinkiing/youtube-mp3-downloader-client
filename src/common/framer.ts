de669d652e7587930dd3287be4c16850dec321ca
Omar Jbara <omar.jbara2@gmail.com>
2019-12-15 00:52:08 +0100

    - Added AnimatePresence for terminal row appearing

--- src/common/framer.ts
+++ src/common/framer.ts
@@ -1,6 +1,6 @@
 import { Variants } from 'framer-motion'
 
-const ease = [0.48, 0.15, 0.25, 0.96]
+export const ease = [0.48, 0.15, 0.25, 0.96]
 
 export const DefaultRouterPageVariants: Variants = {
   exit: { opacity: 0, scale: 0.9, x: -10, transition: { duration: 0.2, ease } },
--- src/components/terminal/row.tsx
+++ src/components/terminal/row.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { AnimatePresence, motion } from 'framer-motion'
+import { ease } from '~/common/framer'
 
-const RowInner = styled.div``
+const RowInner = styled(motion.div)`
+  margin-top: 0.5rem;
+`
 
 const Row: React.FC = ({ children }) => {
   return (
-    <RowInner>
-      {children}
-    </RowInner>
+    <AnimatePresence>
+      <RowInner
+        transition={{ duration: 0.2, ease }}
+        initial={{ opacity: 0, x: -20 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -20 }}
+      >
+        {children}
+      </RowInner>
+    </AnimatePresence>
   )
 }
 
